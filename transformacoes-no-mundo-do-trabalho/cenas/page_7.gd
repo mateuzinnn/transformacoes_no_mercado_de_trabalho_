6a3807cdbd1c385d753909fd5797970ca96139b3
Mateus Almeida <malmeida@fitec.org.br>
2025-12-10 22:41:52 -0300

    fix: alterações

--- transformacoes-no-mundo-do-trabalho/cenas/page_7.gd
+++ transformacoes-no-mundo-do-trabalho/cenas/page_7.gd
@@ -6,18 +6,18 @@ var video_playing = false
 func _ready():
 	$VideoStreamPlayer.stop() # Inicia o vídeo
 
-func _process(delta):
-	if $VideoStreamPlayer.is_playing() == false:
-		print("Vídeo terminou!")
+#func _process(delta):
+	#if $VideoStreamPlayer.is_playing() == false:
+		#print("Vídeo terminou!")
 
-func _input(event):
-	if event is InputEventMouseButton and event.pressed:
-		if not video_playing:
-			audio.stop()
-			$VideoStreamPlayer.play()
-			video_playing = true
-			print("Vídeo começou")
-		else:
-			$VideoStreamPlayer.stop()
-			video_playing = false
-			print("Vídeo parado")
+#func _input(event):
+	#if event is InputEventMouseButton and event.pressed:
+		#if not video_playing:
+			#audio.stop()
+			#$VideoStreamPlayer.play()
+			##video_playing = true
+			#print("Vídeo começou")
+		#else:
+			#$VideoStreamPlayer.stop()
+			#video_playing = false
+			#print("Vídeo parado")
--- transformacoes-no-mundo-do-trabalho/cenas/video_stream_player.gd
+++ transformacoes-no-mundo-do-trabalho/cenas/video_stream_player.gd
@@ -1,11 +1,20 @@
 extends VideoStreamPlayer
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var play_button = get_parent().get_node("Play")
+func _ready():
+	self.stop() # Inicia o vídeo.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_play_pressed() -> void:
+	if self.is_playing():
+		self.stop()
+		print("Parando")
+	else:
+		self.play()
+		play_button.modulate = Color(1, 1, 1, 0)
+		print("Tocando")
